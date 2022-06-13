e4463ea1dc610860cdc758d7f2d198da4b84b98f
samir <samirsamawade@gmail.com>
2022-06-13 14:34:39 +0300

    Jetpack DataStore

---
+++ app/src/main/java/com/samawade/expensetracker/data/UserPreferences.kt
@@ -0,0 +1,47 @@
+package com.samawade.expensetracker.data
+
+import android.content.Context
+import androidx.datastore.DataStore
+import androidx.datastore.preferences.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserPreferences(
+    context: Context
+) {
+    private val applicationContext = context.applicationContext
+    private val dataStore: DataStore<Preferences>
+
+    init {
+        dataStore = applicationContext.createDataStore(
+            name = "my_data_store"
+        )
+    }
+
+    val authToken: Flow<String?>
+        get() = dataStore.data.map { preferences->
+            preferences[KEY_TOKEN]
+            preferences[KEY_ID]
+        }
+
+    suspend fun saveAuthToken(authToken: String, authId: String){
+        dataStore.edit { preferences->
+            preferences[KEY_TOKEN] = authToken
+            preferences[KEY_ID] = authId
+        }
+    }
+
+    suspend fun clear(){
+        dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+
+
+    companion object{
+        private val KEY_TOKEN=  preferencesKey<String>("key_token")
+        private val KEY_ID =  preferencesKey<String>("key_id")
+    }
+
+
+}
---
+++ app/src/main/java/com/samawade/expensetracker/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.samawade.expensetracker.util
+
+class Constants {
+    companion object{
+        const val KEY_PREFERENCES = "expense_preferences"
+        const val KEY_TOKEN = "token"
+        const val KEY_ID = "id"
+    }
+}
